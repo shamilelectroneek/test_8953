3fff86aa13cdde0181e4bb5b82d9a497c6302079
shamilelectroneek <95355965+shamilelectroneek@users.noreply.github.com>
2022-08-26 15:59:24 +0600

    v2
    the timer is set from 0 to 100000. It should work well.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var random_num=Math.floor(Math.random() * 10000);
-console.log(random_num);
-setTimeout(function() {
-  $('table').append("  <tr><td class='element_that_we_need'>Alter Automaion</td><td class='first_name'>Kadyrbek Shamil</td><td class='country'>Kyrgyzstan</td></tr>");
-}, random_num);
+let random_num=Math.floor(Math.random() * 100000);
+console.log('Initial timer: '+random_num);
+$(document).on('click', '#reset_timer', function() {
+    random_num=Math.floor(Math.random() * 100000);
+    console.log('New timer: '+random_num);
+});
+$(document).on('click', '#load_data', function() {
+    setTimeout(function() {
+      $('table').append("  <tr><td class='element_that_we_need'>Alter Automaion</td><td class='first_name'>Kadyrbek Shamil</td><td class='country'>Kyrgyzstan</td></tr>");
+    }, random_num);
+    console.log('Loading data...')
+});
+    
